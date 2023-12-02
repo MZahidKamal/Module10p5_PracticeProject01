90b54e813d3cf1533245605925f0ecc868653eb3
MZahidKamal <kamal.md.zahid@gmail.com>
2023-12-02 14:28:37 +0100

    Daily Learning & Commit

--- Module10p5_PracticeProject01/urls.py
+++ Module10p5_PracticeProject01/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('practice_app/', include('practice_app.urls')),
+    path('', include('practice_app.urls')),
 ]
--- practice_app/urls.py
+++ practice_app/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from .views import home, tasks, geeksforgeeks, earthly, navigations
 
 urlpatterns = [
-    path('', home),
-    path('home/', home),
-    path('tasks/', tasks),
-    path('geeksforgeeks/', geeksforgeeks),
-    path('earthly/', earthly),
-    path('navigations/', navigations),
+    path('', home, name='home'),
+    path('home/', home, name='home'),
+    path('tasks/', tasks, name='tasks'),
+    path('geeksforgeeks/', geeksforgeeks, name='geeksforgeeks'),
+    path('earthly/', earthly, name='earthly'),
+    path('navigations/', navigations, name='navigations'),
 ]
